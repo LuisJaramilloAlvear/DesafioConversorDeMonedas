27de229a662eceb7078a75645d3d4c9ee58e5708
LuisJaramilloAlvear <luis27jaramillo@gmail.com>
2024-11-07 22:57:25 -0300

    Manejar entradas no válidas en el convertidor de moneda
    Agregue bloques try-catch para manejar `InputMismatchException` para la entrada del usuario, asegurando que el programa solo acepte valores enteros y dobles válidos. Esto mejora la solidez de la captura de entradas del usuario y evita fallos debido a entradas no válidas.

--- src/cl/antol/conversor/principal/Principal.java
+++ src/cl/antol/conversor/principal/Principal.java
@@ -6,6 +6,7 @@ import cl.antol.conversor.utilidades.Titulos;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -14,7 +15,6 @@ public class Principal {
 
         // Crear e inicializar el Map con códigos de moneda y descripciones, esta opcion de codigo te permite agregar
         // Dinamicamente mas monedas que sean validas dentro del JSON que trae la API
-
         HashMap<String, String> monedas = new HashMap<>();
         monedas.put("USD", "Dólar EEUU");
         monedas.put("CLP", "Peso Chileno");
@@ -25,7 +25,6 @@ public class Principal {
         monedas.put("BRL", "Real Brasil");
         monedas.put("INR", "Rupia India");
 
-
         // Instanciar el generador de archivos
         GeneradorDeArchivo generadorDeArchivo = new GeneradorDeArchivo();
         String nombreArchivo = "resultado_conversion.txt";
@@ -33,27 +32,35 @@ public class Principal {
         Titulos titulos = new Titulos();
         titulos.mostrarTitulo("Conversor de Monedas");
 
-
-
         // Ciclo principal
-
         while (true) {
             // Mostrar menú con descripciones detalladas y opción de salir
             System.out.println("\nSeleccione el tipo de Moneda:\n");
-            int index = 1;
+            int index = 1; // Usar para enumerar las monedas
             for (String codigo : monedas.keySet()) {
                 System.out.println(index + ") " + codigo + " - " + monedas.get(codigo));
                 index++;
             }
             System.out.println(index + ") Salir");
 
-            // Capturar la opción del usuario
-            int opcion = scanner.nextInt();
+            int opcion = 0;
+            while (true) {
+                try {
+                    System.out.print("Elige una opción: ");
+                    opcion = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, introduce un número entero.");
+                    scanner.next(); // Limpiar la entrada no válida del scanner
+                }
+            }
+
             if (opcion == index) {
                 // Opción de salir
                 System.out.println("Saliendo...");
                 break;
             }
+
             if (opcion <= 0 || opcion > monedas.size()) {
                 // Opción no válida
                 System.out.println("Opción no válida, intente nuevamente.");
@@ -62,8 +69,17 @@ public class Principal {
 
             // Seleccionar moneda de origen
             String monedaOrigen = (String) monedas.keySet().toArray()[opcion - 1];
-            System.out.println("Ingrese la cantidad en " + monedas.get(monedaOrigen) + ":");
-            double cantidad = scanner.nextDouble();
+            double cantidad = 0;
+            while (true) {
+                try {
+                    System.out.println("Ingrese la cantidad en " + monedas.get(monedaOrigen) + ":");
+                    cantidad = scanner.nextDouble();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, introduce un número válido.");
+                    scanner.next(); // Limpiar la entrada no válida del scanner
+                }
+            }
 
             // Mostrar menú para moneda destino y opción de salir
             System.out.println("Indique a qué tipo desea convertir: ");
@@ -74,13 +90,23 @@ public class Principal {
             }
             System.out.println(index + ") Salir");
 
-            // Capturar la opción del usuario
-            opcion = scanner.nextInt();
+            while (true) {
+                try {
+                    System.out.print("Elige una opción: ");
+                    opcion = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, introduce un número entero.");
+                    scanner.next(); // Limpiar la entrada no válida del scanner
+                }
+            }
+
             if (opcion == index) {
                 // Opción de salir
                 System.out.println("Saliendo...");
                 break;
             }
+
             if (opcion <= 0 || opcion > monedas.size()) {
                 // Opción no válida
                 System.out.println("Opción no válida, intente nuevamente.");
@@ -90,8 +116,7 @@ public class Principal {
             // Seleccionar moneda de destino
             String monedaDestino = (String) monedas.keySet().toArray()[opcion - 1];
 
-// Lógica de conversión
-
+            // Lógica de conversión
             ExchangerateAPI consulta = new ExchangerateAPI();
             try {
                 double tasaDeCambio = consulta.obtenerTasaDeCambio(monedaOrigen, monedaDestino);
